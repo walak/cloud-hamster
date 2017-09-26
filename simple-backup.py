1547ce1ce71c9f758fb798f0aa9e522fa3a24880
walak <feniksy@gmail.com>
2017-09-26 18:17:29 +0200

    #2 [REQUIREMENT] As a USER I need the script to compress files

--- simple-backup.py
+++ simple-backup.py
@@ -1,5 +1,10 @@
+import random
+import string
 from json import load
 from sys import argv
+from zipfile import ZipFile, ZIP_DEFLATED
+from os.path import join
+from os import walk, path
 
 CONFIG = {}
 
@@ -9,5 +14,23 @@ def load_config(path):
         config = load(file_handler)
         return config
 
+
+def _generate_random_filename():
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20))
+
+
+def _get_temp_file():
+    return join(CONFIG['temp_directory'], "hamster_" + _generate_random_filename())
+
+
+def compress_folder(path, output_file):
+    with ZipFile(file=output_file, mode="w", compression=ZIP_DEFLATED) as zip_file:
+        for root, directory, files in walk(path):
+            for file in files:
+                zip_file.write(filename=join(root, file),
+                               arcname=join(root.replace(path, ""), file))
+
+
 CONFIG = load_config(argv[1])
 
+compress_folder(CONFIG['backup_directory'], _get_temp_file());
