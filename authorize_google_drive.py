ab4f7dae447ae38f0de2df3e66d8af9d59a60b79
walak <feniksy@gmail.com>
2017-10-03 20:26:01 +0200

    #7 [POC] Check how to authorize command line application

--- authorize_google_drive.py
+++ authorize_google_drive.py
@@ -1,6 +1,9 @@
+from googleapiclient.discovery import build
 from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.tools import argparser
 
 from model import OAuthSettings
+import httplib2
 
 settings = OAuthSettings.load_from_file("app.json")
 
@@ -9,9 +12,16 @@ flow = OAuth2WebServerFlow(client_id=settings.client_id,
                            scope=settings.scope,
                            redirect_uri=settings.redirect_uri)
 
-print("Go to %s and copy received key: " % flow.step1_get_authorize_url())
-key = input()
+flags = argparser.parse_args(["--noauth_local_webserver"])
 
-credentials = flow.step2_exchange(code=key)
+auth_url = flow.step1_get_authorize_url()
 
-print("OK@")
+print("Go to %s and paste received code here:" % auth_url)
+code = input()
+
+credentials = flow.step2_exchange(code=code)
+transport = httplib2.Http()
+
+service = build('drive', 'v3', credentials=credentials)
+result = service.about().get(fields="storageQuota").execute()
+print(result)
